e8714e313c0285b27fec2b87566948fc0b9e07e1
sakuexe <saku.karttunen@gmail.com>
2024-03-27 08:57:28 +0200

    started work on the mongodb assingment

--- databased-vs/Controllers/HomeController.cs
+++ databased-vs/Controllers/HomeController.cs
@@ -4,39 +4,49 @@ using System.Diagnostics;
 
 namespace databased_vs.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly List<string> _names = new List<string>(){
+            "Pertti", "Paavo", "Maksimus", "Saku", "Sienimies", "Amogustus",
+            "Jeso'n", "Patrik", "Frank", "Matt", "Homer",
+        };
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-		public IActionResult Index()
-		{
-			var data = DatabaseManipulator.GetAll<User>("User");
-			return View(data);
-		}
+        public IActionResult Index()
+        {
+            var data = DatabaseManipulator.GetAll<User>("User");
+            return View(data);
+        }
 
-		public IActionResult Privacy()
-		{
-			var testi = new User()
-			{
-				Name = "Testo Teppo",
-				Email = "Testimies@testing.com",
-				Password = "1234",
-			};
-			User user = DatabaseManipulator.Save<User>("User", testi);
-			testi.Name = "Matti Nykänen";
-			user = DatabaseManipulator.Save<User>("User", testi);
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            var rnd = new Random();
+            var randomName = _names[rnd.Next(0, _names.Count)];
+            /* var testi = new User() */
+            /* { */
+            /*     Name = randomName, */
+            /*     Email = $"{randomName}{rnd.Next(100)}@testing.com", */
+            /*     Password = $"pswrd_{rnd.Next(1000)}_{rnd.Next(200)}#", */
+            /* }; */
+            User testi = new()
+            {
+                Name = "sakuk",
+                Email = "susamogus",
+                Password = "safeword",
+            };
+            User user = DatabaseManipulator.Save<User>(testi);
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
